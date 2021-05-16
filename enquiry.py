16661885a4417e02fa7082eb40cf3cd07f1dfad9
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-05-16 23:28:50 +0530

    Add files via upload

---
+++ enquiry.py
@@ -0,0 +1,13 @@
+a=str(input("enter the course name:"))
+if a=="python":
+    print("duration:45 days")
+    print("fee:15k")
+elif a=="java":
+    print("duration:45 days")
+    print("fee:12k")
+elif a=="ML":
+    print("duration:40 days")
+    print("fee:10k")    
+else:
+    print("Sorry this course is currently not available")
+        
