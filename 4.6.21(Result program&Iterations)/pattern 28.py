c679ee9db72bd6711db35bfacbd893fd3dff372b
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-06-03 23:57:43 +0530

    Add files via upload

---
+++ 4.6.21(Result program&Iterations)/pattern 28.py
@@ -0,0 +1,48 @@
+#pattern 28
+for i in range(69,64,-1):
+    for j in range(5):
+        print(chr(i),end=" ")
+    print("")
+
+ ##ITERATION
+##
+##i       j     print
+##
+##
+##69       1       E
+##69       2       E
+##69       3       E
+##69       4       E
+##69       5       E
+##
+##68       1       D
+##68       2       D
+##68       3       D
+##68       4       D
+##68       5       D
+##
+##67       1       C
+##67       2       C
+##67       3       C
+##67       4       C
+##67       5       C
+##
+##66       1       B
+##66       2       B
+##66       3       B
+##66       4       B
+##66       5       B
+##
+##65       1       A
+##65       2       A
+##65       3       A
+##65       4       A
+##65       5       A
+
+##OUTPUT
+##
+##E E E E E 
+##D D D D D 
+##C C C C C 
+##B B B B B 
+##A A A A A
