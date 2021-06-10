2d11a7e999f2c8d7e7f0ae055c15b0b656aaef6e
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-06-10 16:22:11 +0530

    Delete uppercase for first and last letter in a string.py

--- 10.6.21(methods program)/uppercase for first and last letter in a string.py
+++
@@ -1,9 +0,0 @@
-#Python program to display the first and last letter in a string in capital letters
-a=input("Enter the String:")
-a=a.title()
-w=a.split()
-r=""
-for i in w:
-    r=r+i[:-1]+i[-1].upper()+" "
-print("New string:",r[:-1])
-
