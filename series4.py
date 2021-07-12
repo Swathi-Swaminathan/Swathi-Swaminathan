7768be1fccea76e04acb98e4c0721254ee35997f
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-07-12 16:01:53 +0530

    Delete series4.py

--- series4.py
+++
@@ -1,9 +0,0 @@
-#python program to print series: 53, 53, 40, 40, 27, 27, ...
-n=int(input())
-print(n,end=",")
-for i in range(n):
-    a=n
-    b=n-13
-    print(str(a)+","+str(b),end=",")
-    n=b
-    a=b
