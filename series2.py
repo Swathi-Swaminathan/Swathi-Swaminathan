8d21461c40efd6cd9fc224af2919abf9f220d62b
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-07-12 16:02:19 +0530

    Delete series2.py

--- series2.py
+++
@@ -1,9 +0,0 @@
-#python program to print series: 36, 34, 30, 28, 24, ..
-n=int(input())
-print(n,end=",")
-for i in range(n):
-    a=n-2
-    b=n-6
-    print(str(a)+","+str(b),end=",")
-    n=b
-    a=b
