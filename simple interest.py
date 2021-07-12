dbb5103214a1b5398a34282f05a6f3e2694cf1d4
Swathi-Swaminathan <84173458+Swathi-Swaminathan@users.noreply.github.com>
2021-07-12 16:01:28 +0530

    Delete simple interest.py

--- simple interest.py
+++
@@ -1,6 +0,0 @@
-#find simple interest
-p=float(input("enter the principle amount:"))
-t=float(input("enter the time:"))
-r=float(input("enter the rate:"))
-si=p*t*r/100
-print("simple interest is:",si)
